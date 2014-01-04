27e3455b870d4f54e2f73b6feebb78cf4b5db750
michal915 <michalkielan6@gmail.com>
2014-01-04 21:04:57 +0100

    AVR 74HC595

---
+++ main.c
@@ -0,0 +1,67 @@
+/* Shif register 74HC595
+ *
+ *
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/iom2560.h>
+#include <util/delay.h>
+
+#define SCK PB1 	//pin 52 - 11 SHCP - CLK
+#define MOSI PB2	//pin 51 - 14 DS - Serial Data
+#define LT PB0 		//pin 53 - 12 STCP - Latch Clock
+
+#define LT_ON PORTB |= (1<<PB0)
+#define LT_OFF PORTB &= ~(1<<PB0)
+
+void InitSpi(void);
+void SendSpi(uint8_t bajt);
+
+int main()  {
+
+	InitSpi();
+
+	SendSpi(0xF0);
+
+
+
+	while(1)  {
+		/*uint8_t data = 1;
+
+		while(data)	{
+			SendSpi(data);
+			_delay_ms(100);
+
+			data <<= 1;
+		}
+
+		data = 0x80;
+
+		while(data)	{
+			SendSpi(data);
+			_delay_ms(50);
+
+			data >>=1;
+		}
+*/
+
+
+	}
+}
+
+void InitSpi(void)	{
+
+	DDRB |= (1<<SCK) | (1<<MOSI) | (1<<LT);
+	SPCR |= (1<<SPE) | (1<<MSTR) | (1<<SPR1);
+}
+
+
+void SendSpi(uint8_t bajt)	{
+
+	SPDR = bajt;
+	while( !(SPSR & (1<<SPIF)) );
+
+	LT_ON;
+	LT_OFF;
+}
